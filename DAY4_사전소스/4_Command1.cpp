a8b52f27c1fbdfdba1e867f193704b2701887ff8
leeh8911@gmail.com <54252269+leeh8911@users.noreply.github.com>
2022-07-14 15:11:10 +0900

    update

--- DAY4_사전소스/4_Command1.cpp
+++ DAY4_사전소스/4_Command1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include "Helper.h"
 
 class Shape
@@ -28,10 +29,56 @@ struct ICommand {
 	virtual void Undo() {}
 	virtual ~ICommand() {}
 };
+
+// 사각형을 추가하는 명령
+class AddRectCommand : public ICommand {
+	std::vector<Shape*>& v;
+public:
+	AddRectCommand(std::vector<Shape*>& v) : v(v) {}
+	void Execute() override { v.push_back(new Rect); }
+
+	bool CanUndo() override { return true; }
+	void Undo() override {
+		Shape* p = v.back();
+		v.pop_back();
+
+		delete p;
+	}
+};
+
+
+class AddCircleCommand : public ICommand {
+	std::vector<Shape*>& v;
+public:
+	AddCircleCommand(std::vector<Shape*>& v) : v(v) {}
+	void Execute() override { v.push_back(new Circle); }
+
+	bool CanUndo() override { return true; }
+	void Undo() override {
+		Shape* p = v.back();
+		v.pop_back();
+
+		delete p;
+	}
+};
+
+class DrawCommand : public ICommand {
+	std::vector<Shape*>& v;
+public:
+	DrawCommand(std::vector<Shape*>& v) : v(v) {}
+	void Execute() override { for (auto p : v) p->draw(); }
+
+	bool CanUndo() override { return true; }
+	void Undo() override {
+		system("cls");
+	}
+};
+
 int main()
 {
 	std::vector<Shape*> v;
-
+	ICommand* pCmd = nullptr;
+	std::stack<ICommand*> undo_stack;
 	while (1)
 	{
 		int cmd;
@@ -39,17 +86,36 @@ int main()
 
 		if (cmd == 1)
 		{
-			v.push_back(new Rect);
+			pCmd = new AddRectCommand(v);
+			pCmd->Execute();
+
+			undo_stack.push(pCmd);
 		}
 		else if (cmd == 2)
 		{
-			v.push_back(new Circle);
+			pCmd = new AddCircleCommand(v);
+			pCmd->Execute();
+
+			undo_stack.push(pCmd);
 		}
 		else if (cmd == 9)
 		{
-			for (int i = 0; i < v.size(); i++)
-			{
-				v[i]->draw();
+			pCmd = new DrawCommand(v);
+			pCmd->Execute();
+
+			undo_stack.push(pCmd);
+		}
+		else if (cmd == 0) {
+			if (!undo_stack.empty()) {
+				pCmd = undo_stack.top();
+				undo_stack.pop();
+				if (pCmd->CanUndo()) {
+					pCmd->Undo();
+				
+					delete pCmd;// 여기서 delete 하지 말고,
+								// redo_stack.push(pCmd) 하면
+								// redo도 만들 수 있음
+				}
 			}
 		}
 	}
